5a139119f12688b3d4692dd73670723702ddcd48
aqnya <siyueba73@gmail.com>
2026-04-06 19:15:38 +0800

    load libncore_init.so

--- app/src/main/java/me/nekosu/aqnya/NkApplication.kt
+++ app/src/main/java/me/nekosu/aqnya/NkApplication.kt
@@ -6,8 +6,14 @@ import android.util.Log
 import me.nekosu.aqnya.R
 import me.nekosu.aqnya.util.CrashHandler
 import kotlin.system.exitProcess
+import android.content.Context
 
 class NkApplication : Application() {
+override fun attachBaseContext(base: Context) {
+        System.loadLibrary("ncore_init")
+        super.attachBaseContext(base)
+    }
+
     override fun onCreate() {
         CrashHandler.init(this)
         super.onCreate()
