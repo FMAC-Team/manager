bd73457f15748c84329d1206006bf6d1249b0bee
aqnya <siyueba73@gmail.com>
2026-04-12 01:19:46 +0800

    opz color.

--- flutter/lib/navbar.dart
+++ flutter/lib/navbar.dart
@@ -33,7 +33,7 @@ class ModernCapsuleNavBar extends StatelessWidget {
         child: Container(
           height: 68,
           decoration: BoxDecoration(
-            color: scheme.surface,
+            color: scheme.onSurface,
             borderRadius: BorderRadius.circular(34),
             boxShadow:[
               BoxShadow(
